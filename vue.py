de2e9a5764518678550609ba09dc29de725a446b
Dave Lebrun <dave.lebrun@live.ca>
2012-08-30 23:36:31 -0400

    Edit 08/30 23:37 Please enter the commit message for your changes. Lines starting

--- vue.py
+++ vue.py
@@ -1,8 +1,27 @@
+########################################
+#AUTEUR: David, Samuel, Jean-Sebastien
+#TITRE: REDSQUARE
+#DATE DE REMISE: 30 AOUT 2012
+########################################
+
 from tkinter import *
-import modele.py
-import controlleur.py
 
 class RenduCarte():
-    
-    def __init__(self):
+
+    def __init__(self, parent = None):
+        return None
         
+    def affichePanel(self):
+        frame = Frame(width=400, height=400, colormap="new")
+        frame.pack() 
+    
+    def afficheJeux(self):
+        fenetre = Tk()
+        rc.affichePanel()
+        fenetre.mainloop()
+         
+
+if __name__ == '__main__':
+    rc = RenduCarte()
+    rc.afficheJeux()
+    
