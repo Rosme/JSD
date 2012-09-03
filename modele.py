514ab603a8941e9bc251702e8e87f21bf1b0e1de
Samuel Ryc <samuel.ryc@gmail.com>
2012-09-03 16:05:49 -0300

    Update modele.py
    Un début de ma classe RedSquare.

--- modele.py
+++ modele.py
@@ -1,5 +1,4 @@
 
-
 class Bornes():
     def __init__(self, x, y, longueur, hauteur):
         self.x = x
@@ -9,3 +8,18 @@ class Bornes():
         
 class Formes():
     def __init__(self):
+        
+class RedSquare():
+    def __init__(self):
+        self.x = 50 #position x du RedSquare au départ (à modifier eventuellement)
+        self.y = 50 #position y du RedSquare au départ (à modifier eventuellement)
+        self.longueur = 20 #longueur du RedSquare
+        self.hauteur = 20 #largeur du RedSquare        
+        
+    def getBornes(self):
+        self.bornesRS = Bornes(x, y, longueur, hauteur) #RS pour RedSquare (abréviation)
+        
+    def deplacer(x, y):
+        #bornesRS.x = get coordonnée x de la souris
+        #bornesRS.y = get coordonnée y de la souris
+        
