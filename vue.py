4997ba00c5002e365958fcbfe7fddddacda722ef
Dave Lebrun <dave.lebrun@live.ca>
2012-09-03 16:26:00 -0400

    Affichage du panel + organiser + centrer

--- vue.py
+++ vue.py
@@ -3,24 +3,37 @@ from tkinter import *
 class RenduCarte():
 
     def __init__(self):
-        return None                 ###
+        return None                 
     
-    def afficherFormes(self):
-        return None                 ###
+    def centrerFenetre(self,w=600, h=500):              #Permet de centrer l'application au centre de l'ecran
+        ws = self.fenetre.winfo_screenwidth()
+        hs = self.fenetre.winfo_screenheight()
+        x = (ws/2) - (w/2)    
+        y = (hs/2) - (h/2)
+        self.fenetre.geometry('%dx%d+%d+%d' % (w, h, x, y))
     
-    def dessinerPanel(self):
-        frame = Frame(width=400, height=400, colormap="new")
-        frame.pack() 
+    def afficherFormes(self):                           #affichages des formes dans le panelSecondaire
+        return None                 
     
-    def afficherCarte(self):
-        fenetre = Tk()
-        rc.dessinerPanel()
-        fenetre.mainloop()
+    def panelPrincipal(self):                           #Contient tout ce qui a dans la fenetre de tkinter  
+        self.FenetrePrincipal = Frame(width=600, height=500, bd = 2, colormap="new", relief = 'groove', bg = "black")
+    
+    def panelSecondaire(self):                          #Contient le jeux 
+        self.FenetreSecondaire = Frame(width = 450, height = 380, bd = 2, colormap = "new", relief = 'groove', bg = "white")
+    
+    def afficherCarte(self):                            #Permet d'afficher les widgets du jeux et de packer les widgets
+        self.fenetre = Tk()
+        rc.panelPrincipal()
+        rc.panelSecondaire()
+        self.FenetrePrincipal.pack()
+        self.FenetreSecondaire.place(in_=self.FenetrePrincipal, anchor="c", relx=.4, rely=.4)
+        rc.centrerFenetre()
+        self.fenetre.mainloop()
         
 class RenduInterface():
     
     def __init__(self):
-        return None                 ###
+        return None                 
     
     def afficherBouton(self):
         return None
