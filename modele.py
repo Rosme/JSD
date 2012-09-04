06ad0210235c5a0b83539dfbd4b34af06df02be0
Jean-Sebastien Fauteux <jeanseb.fauteux@gmail.com>
2012-09-03 22:36:57 -0400

    Systeme de collision

--- modele.py
+++ modele.py
@@ -68,3 +68,32 @@ class Temps():
             return str(self.time.seconds)
         else:
             return str(self.time.seconds)
+        
+class GerantCollision():
+    #Methode de verification pour les collisions entre formes
+    def collisionForme(self, bornesA, bornesB):
+        bornesGauche = [bornesA.x, bornesB.x]
+        bornesTop = [bornesA.y, bornesB.y]
+        bornesDroite = [bornesA.x+bornesA.longueur, bornesB.x+bornesB.longueur]
+        bornesBas = [bornesA.y+bornesA.hauteur, bornesB.y+bornesB.hauteur]
+        interGauche = max(bornesGauche)
+        interTop = max(bornesTop)
+        interDroite = min(bornesDroite)
+        interBas = min(bornesBas)
+        
+        #Si on a une collision, on retourne vrai, sinon faux
+        if interGauche < interDroite and interTop < interBas:
+            return True
+        else:
+            return False
+    
+    #Methode de verification de collision pour les mur exterieur
+    def collisionExterieur(self, bornesA, bornesB): #On assume que bornesA est le joueur
+        if bornesA.x <= bornesB.x or bornesA.y <= bornesB.y: #On est sorti en haut ou a gauche
+            return True
+        if bornesA.x+bornesA.longueur >= bornesB.x+bornesB.longueur: #On est sorti a droite
+            return True
+        if bornesA.y+bornesA.hauteur >= bornesB.y+bornesB.hauteur: #On est sorti en bas
+            return True
+        return False # On est toujours a l'interieur
+        
