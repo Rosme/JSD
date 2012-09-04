f0f78614ba636af425558c22ae25ff57808addee
Dave Lebrun <dave.lebrun@live.ca>
2012-09-04 14:19:41 -0400

    Ajout boutons et de leur fonction

--- vue.py
+++ vue.py
@@ -1,53 +1,48 @@
+'''
+vue.py
+Fichier contenant les classes vue du Redsquare
+Projet par Jean-Sebastien Fauteux, Samuel Ryc et David Lebrun
+'''
+##-*- coding: ISO-8859-1 -*-
 from tkinter import *
+from tkinter import messagebox
 
 class RenduCarte():
 
     def __init__(self):
-        return None                 
+        self.root = Tk()  
+        self.root.title("RedSquare - Projet B41")  
+        self.fenPrincipale=Frame(width=600, height=500, bd = 2, colormap="new", relief = 'groove', bg = "black")
+        self.fenSecondaire=Frame(width=450,height=380, bd=2,colormap="new", relief='groove', bg="white")
+        self.fenBoutton=Frame(bd=2, colormap="new")
+        self.bouttonInfo=Button(self.fenBoutton, text="Informations", padx=20, command=self.callback)
+        self.bouttonQuit=Button(self.fenBoutton, text="Quitter", padx=35, command=self.root.quit)
+        
+    def callback(self):
+        messagebox.showinfo("Informations", "Developpe par :\nJean-Sebatien Fauteux\nSamuel Ryc\nDavid Lebrun")
+        
+    def afficherFenetre(self):                         #Permet d'afficher les widgets du jeux et de packer les widgets
+        renduOption = RenduOption()
+        self.centrerFenetre()
+        self.fenPrincipale.pack()
+        self.fenSecondaire.place(in_=self.fenPrincipale, anchor="c", relx=.4, rely=.4)
+        self.fenBoutton.place(in_=self.fenPrincipale,relx=.1, rely=.8)
+        self.bouttonInfo.pack()
+        self.bouttonQuit.pack()
     
     def centrerFenetre(self,w=600, h=500):              #Permet de centrer l'application au centre de l'ecran
-        ws = self.fenetre.winfo_screenwidth()
-        hs = self.fenetre.winfo_screenheight()
+        ws = self.root.winfo_screenwidth()
+        hs = self.root.winfo_screenheight()
         x = (ws/2) - (w/2)    
         y = (hs/2) - (h/2)
-        self.fenetre.geometry('%dx%d+%d+%d' % (w, h, x, y))
-    
-    def afficherFormes(self):                           #affichages des formes dans le panelSecondaire
-        return None                 
-    
-    def panelPrincipal(self):                           #Contient tout ce qui a dans la fenetre de tkinter  
-        self.FenetrePrincipal = Frame(width=600, height=500, bd = 2, colormap="new", relief = 'groove', bg = "black")
-    
-    def panelSecondaire(self):                          #Contient le jeux 
-        self.FenetreSecondaire = Frame(width = 450, height = 380, bd = 2, colormap = "new", relief = 'groove', bg = "white")
-    
-    def afficherCarte(self):                            #Permet d'afficher les widgets du jeux et de packer les widgets
-        self.fenetre = Tk()
-        self.fenetre.title("RedSquare - Projet B41")
-        self.panelPrincipal()
-        self.panelSecondaire()
-        self.FenetrePrincipal.pack()
-        self.FenetreSecondaire.place(in_=self.FenetrePrincipal, anchor="c", relx=.4, rely=.4)
-        self.centrerFenetre()
+        self.root.geometry('%dx%d+%d+%d' % (w, h, x, y))                
         
-class RenduInterface():
+class RenduOption():                #A revenir plus tard.... gestion des boutons
     
     def __init__(self):
         return None
-    
-    def bouttonQuitter(self):
-        self.BouttonQuitter = Button(rc.FenetrePrincipal, text = "Quitter", bg = "white", command = rc.FenetrePrincipal.quit)
-        #self.BouttonQuitter.pack()                    #A revoir, quand pack() change l'interface et bouton ne s'affiche pas
-    
-    def bouttonInfo(self):
-        return None
-    
-    def afficherBoutton(self):
-        self.bouttonQuitter()
 
-if __name__ == '__main__':         
-    rc = RenduCarte()
-    ri = RenduInterface()
-    rc.afficherCarte()
-    ri.afficherBoutton()
-    rc.fenetre.mainloop()       #Demarre le gestionnaire d'evenements
+if __name__ == '__main__':       
+    renduCarte = RenduCarte()
+    renduCarte.afficherFenetre()
+    renduCarte.root.mainloop()       #Demarre le gestionnaire d'evenements
