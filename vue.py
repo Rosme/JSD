bdadf302cf26c0c50226afb8c7b0bd8a6806bcb9
Dave Lebrun <dave.lebrun@live.ca>
2012-09-05 12:32:47 -0400

    interface

--- vue.py
+++ vue.py
@@ -12,37 +12,58 @@ class RenduCarte():
     def __init__(self):
         self.root = Tk()  
         self.root.title("RedSquare - Projet B41")  
-        self.fenPrincipale=Frame(width=600, height=500, bd = 2, colormap="new", relief = 'groove', bg = "black")
-        self.fenSecondaire=Frame(width=450,height=380, bd=2,colormap="new", relief='groove', bg="white")
+        self.fenPrincipale=Frame(width=600, height=500, bd = 2, colormap="new", relief = 'groove', bg = "black")    #contient frame du jeux, bouton et eventuellement frame pour highscore
+        self.aireJeux=Canvas(width=450,height=370, bd=2, bg="white")            #contient le jeux
         self.fenBoutton=Frame(bd=2, colormap="new")
-        self.bouttonInfo=Button(self.fenBoutton, text="Informations", padx=20, command=self.callback)
-        self.bouttonQuit=Button(self.fenBoutton, text="Quitter", padx=35, command=self.root.quit)
-        
-    def callback(self):
-        messagebox.showinfo("Informations", "Developpe par :\nJean-Sebatien Fauteux\nSamuel Ryc\nDavid Lebrun")
-        
-    def afficherFenetre(self):                         #Permet d'afficher les widgets du jeux et de packer les widgets
+
+    ###Dessine les rectangles (ennemies) dans le canvas
+    def dessinerFormes(self):
+        self.rectangleHautGauche = self.aireJeux.create_rectangle(10,10,60,125, fill="blue")
+        self.rectangleBasGauche = self.aireJeux.create_rectangle(10,300,150,350, fill="blue")
+        self.rectangleBasDroite = self.aireJeux.create_rectangle(390,350,440,200, fill="blue")
+        self.rectangleHautDroite = self.aireJeux.create_rectangle(250,20,440,70, fill="blue")
+    
+    ###Dessinge le redsquare dans le canvas
+    def dessinerRedsquare(self):
+        self.redsquare = self.aireJeux.create_rectangle(200,150,250,200, fill="red") 
+    
+    ###Permet d'afficher les widgets du jeux et de packer les widgets   
+    def afficherFenetre(self):                   
         renduOption = RenduOption()
         self.centrerFenetre()
         self.fenPrincipale.pack()
-        self.fenSecondaire.place(in_=self.fenPrincipale, anchor="c", relx=.4, rely=.4)
-        self.fenBoutton.place(in_=self.fenPrincipale,relx=.1, rely=.8)
-        self.bouttonInfo.pack()
-        self.bouttonQuit.pack()
-    
-    def centrerFenetre(self,w=600, h=500):              #Permet de centrer l'application au centre de l'ecran
+        self.aireJeux.place(in_=self.fenPrincipale, anchor="c", relx=.4, rely=.4)
+        self.dessinerFormes()
+        self.dessinerRedsquare()
+        self.fenBoutton.place(in_=self.fenPrincipale,relx=.1, rely=.8) 
+        renduOption.afficherBouton()
+        
+    ###Permet de centrer l'application au centre de l'ecran
+    def centrerFenetre(self,w=600, h=500):              
         ws = self.root.winfo_screenwidth()
         hs = self.root.winfo_screenheight()
         x = (ws/2) - (w/2)    
         y = (hs/2) - (h/2)
         self.root.geometry('%dx%d+%d+%d' % (w, h, x, y))                
         
-class RenduOption():                #A revenir plus tard.... gestion des boutons
+class RenduOption():
     
     def __init__(self):
-        return None
+        self.bouttonInfo=Button(renduCarte.fenBoutton, text="Informations", padx=60, command=self.afficherInfo)
+        self.bouttonQuit=Button(renduCarte.fenBoutton, text="Quitter", padx=60, command=renduCarte.root.quit)
+    
+    
+    ###Affiche les informations si l'utilisateur clique sur le bouton informations. fonction de la command du boutton    
+    def afficherInfo(self):
+        messagebox.showinfo("Informations", "Developpe par :\nJean-Sebatien Fauteux\nSamuel Ryc\nDavid Lebrun")
+        
+    ###Affiche les bouton a partir de afficherFenetre dans RenduCarte()
+    def afficherBouton(self):
+        self.bouttonInfo.pack(side=LEFT)
+        self.bouttonQuit.pack(side=LEFT)
 
 if __name__ == '__main__':       
-    renduCarte = RenduCarte()
+    renduCarte = RenduCarte()     
+    renduOption = RenduOption()
     renduCarte.afficherFenetre()
-    renduCarte.root.mainloop()       #Demarre le gestionnaire d'evenements
+    renduCarte.root.mainloop()          
