3f128ccc9ef536b2e7d32e345bbd20046a483f7d
Rosme <jeanseb.fauteux@gmail.com>
2012-09-05 22:46:30 -0400

    Controlleur basic_ non fonctionnel

--- controlleur.py
+++ controlleur.py
@@ -1,6 +1,6 @@
 '''
-vue.py
-Fichier contenant les classes vue du Redsquare
+controlleur.py
+Fichier contenant les classes controlleur du Redsquare
 Projet par Jean-Sebastien Fauteux, Samuel Ryc et David Lebrun
 '''
 ##-*- coding: ISO-8859-1 -*-
@@ -9,4 +9,14 @@ import modele
 import vue
 
 class Jeu():
+    def __init__(self):
+        self.interface = vue.RenduInterface(self)
+        self.interface.setData(self.rouge, self.forme)
     
+    def run(self):
+        self.interface.dessiner()
+        
+    
+if __name__ == '__main__':
+    j = Jeu()
+    j.run()
