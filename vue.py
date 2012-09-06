b2b80a3aba26fdefb63df4a8987705d2ac3aeba7
Rosme <jeanseb.fauteux@gmail.com>
2012-09-06 00:30:43 -0400

    Vue update

--- vue.py
+++ vue.py
@@ -7,36 +7,44 @@ Projet par Jean-Sebastien Fauteux, Samuel Ryc et David Lebrun
 from tkinter import *
 from tkinter import messagebox
 
+import controlleur
+
 class RenduCarte():
 
     def __init__(self, parent, redsquare, formes):
         self.parent = parent
         self.redsquare = redsquare
         self.formes = formes
-        self.canvas=Canvas(width=450,height=370, bd=2, bg="white")            #contient le jeux
+        self.bleu = list()
+        self.canvas=Canvas(width=375,height=375, bd=2, bg="white")            #contient le jeux
         borneRedSquare = redsquare.getBornes()
-        self.rouge = self.canvas.create_rectangle(borneRedSquare.x,borneRedSquare.y,borneRedSquare.longueur,borneRedSquare.hauteur, fill=redsquare.getCouleur()) 
+        self.rouge = self.canvas.create_rectangle(borneRedSquare.x,borneRedSquare.y,borneRedSquare.x+borneRedSquare.longueur,borneRedSquare.y+borneRedSquare.hauteur, fill=redsquare.getCouleur())
+        #self.canvas.delete(self.rouge)
+        #self.canvas.update() 
 
     ###Dessine les rectangles (ennemies) dans le canvas
     def dessinerFormes(self):
-        '''
-        self.rectangleHautGauche = self.aireJeux.create_rectangle(10,10,60,125, fill="blue")
-        self.rectangleBasGauche = self.aireJeux.create_rectangle(10,300,150,350, fill="blue")
-        self.rectangleBasDroite = self.aireJeux.create_rectangle(390,350,440,200, fill="blue")
-        self.rectangleHautDroite = self.aireJeux.create_rectangle(250,20,440,70, fill="blue")
-        '''
+        for f in self.bleu:
+            self.canvas.delete(f)
+        
+        for f in self.formes:
+            borne = f.getBornes()
+            self.bleu.append(self.canvas.create_rectangle(borne.x, borne.y, borne.x+borne.longueur, borne.y+borne.hauteur, fill=f.getCouleur()))
+        
+        self.canvas.update()
     
     ###Dessinge le redsquare dans le canvas
     def dessinerRedsquare(self):
-        self.canvas.create_rectangle(200,150,250,200, fill="red")
         self.canvas.bind("<B1-Motion>", self.deplacerRedsquare)
     
         ###Permet de deplacer le redsquare
     def deplacerRedsquare(self, event):
-        self.canvas.delete(self.redsquare)
+        self.canvas.delete(self.rouge)
         x = event.x
         y = event.y
-        self.redsquare = self.canvas.create_rectangle(x-25,y-25,x+25,y+25, fill = "red")
+        self.redsquare.deplacer(x,y)
+        borneRedSquare = self.redsquare.getBornes()
+        self.rouge = self.canvas.create_rectangle(x-25,y-25,x+25,y+25, fill=self.redsquare.getCouleur())
         self.canvas.update()
     
     ###Permet d'afficher les widgets du jeux et de packer les widgets   
@@ -45,9 +53,9 @@ class RenduCarte():
         
     ###Permet d'afficher les widgets du jeux et de packer les widgets   
     def afficherFenetre(self):                   
-        self.canvas.place(in_=parent.fenPrincipale, anchor="c", relx=.4, rely=.4)
-        self.dessinerFormes()
+        self.canvas.place(in_=self.parent.fenPrincipale, anchor="c", relx=.4, rely=.4)
         self.dessinerRedsquare()
+        self.dessinerFormes()
         
 class RenduOption():
     
@@ -67,25 +75,26 @@ class RenduOption():
         
 class RenduInterface():
     
-    def __init__(self, parent):
+    def __init__(self, parent, redsquare, formes):
         self.root = Tk()
         self.root.title("RedSquare - Projet B41")
-        self.fenPrincipale=Frame(width=600, height=500, bd = 2, colormap="new", relief = 'groove', bg = "black")    #contient frame du jeux, bouton et eventuellement frame pour highscore
+        self.formes = formes
+        self.parent = parent
+        self.redsquare = redsquare
+        self.fenPrincipale=Frame(width=600, height=600, bd = 2, colormap="new", relief = 'groove', bg = "black")    #contient frame du jeux, bouton et eventuellement frame pour highscore
         self.fenBoutton=Frame(bd=2, colormap="new")
-        self.carte = RenduCarte(self)
+        self.rcCarte = RenduCarte(self, redsquare, formes)
         self.option = RenduOption(self)
+        self.alive = True
         
     def dessiner(self):
         self.centrerFenetre()
         self.fenPrincipale.pack()
         self.fenBoutton.place(in_=self.fenPrincipale,relx=.1, rely=.8)
-        self.carte.afficherFenetre()
-        self.option.afficherBouton()    
+        self.rcCarte.afficherFenetre()
+        self.option.afficherBouton()
+        self.parent.update()
         self.root.mainloop()
-    
-    def setData(self, redsquare, formes):
-        self.carte.redsquare = redsquare
-        self.carte.formes = formes
         
     ###Permet de centrer l'application au centre de l'ecran
     def centrerFenetre(self,w=600, h=500):              
@@ -94,9 +103,20 @@ class RenduInterface():
         x = (ws/2) - (w/2)    
         y = (hs/2) - (h/2)
         self.root.geometry('%dx%d+%d+%d' % (w, h, x, y))
+              
     
+    def gameOver(self):
+        if self.alive:
+            messagebox.showinfo("Vous etes mort!", "Oups! Vous etes mort!")
+            self.alive = False
+            self.reset()
+    
+    def reset(self):
+        self.parent.reset()
+        self.formes = self.parent.niveau.getFormes()
+        self.redsquare = self.parent.niveau.getRouge()
+        self.rcCarte = RenduCarte(self, self.redsquare, self.formes)
+        
 if __name__ == '__main__':       
-    renduCarte = RenduCarte()     
-    renduOption = RenduOption()
-    renduCarte.afficherFenetre()
-    renduCarte.root.mainloop()          
+    j = controlleur.Jeu()
+    j.run()         
