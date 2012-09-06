019e22de23cfab9ba444d074c9143f07b6ccbcae
Jean-Sebastien Fauteux <jeanseb.fauteux@gmail.com>
2012-09-05 21:31:19 -0400

    ENcoding et header controlleur

---
+++ controlleur.py
@@ -0,0 +1,12 @@
+'''
+vue.py
+Fichier contenant les classes vue du Redsquare
+Projet par Jean-Sebastien Fauteux, Samuel Ryc et David Lebrun
+'''
+##-*- coding: ISO-8859-1 -*-
+
+import modele
+import vue
+
+class Jeu():
+    
